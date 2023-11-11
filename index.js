dc889e89b7aee390a002cd67cc6bb24842a7a822
OusD1213 <143454481+OusD1213@users.noreply.github.com>
2023-11-11 16:39:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+// let resetEl = document.getElementById("reset-el")
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+}
+function reset() {
+  count = 0;
+  countEl.textContent = count;
+}
+function deleteData() {
+  let deleteText = "Previous entries:";
+  saveEl.textContent = deleteText;
+}
